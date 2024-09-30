50c70583bd4fd05972225103efb9cce2de4ede4b
VISHALINI <142875791+vishalinijerry@users.noreply.github.com>
2024-09-30 02:18:18 -0700

    Add files via upload

---
+++ jsresid.js
@@ -0,0 +1,13 @@
+// var total=document.getElementById("total");
+// console.log(total);
+// var advance=document.getElementById("adv");
+// console.log(advance);
+// var balance=document.getElementById("bal");
+// console.log(balance);
+// function balance(){
+//     return total-advance;
+// }
+// balance()
+
+
+
